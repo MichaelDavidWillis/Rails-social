414c70e793dbeb2410ee2396c7816ce2997c17ae
Michael Willis <michaeldavidwillisuk@gmail.com>
2024-11-30 22:24:48 +0000

    Add resources to Users & link to show view

--- social-app/config/routes.rb
+++ social-app/config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'user#index'
   
-  get "user", to: 'user#index'
-  get "user/:id", to: 'user#show'
+  resources :user
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
